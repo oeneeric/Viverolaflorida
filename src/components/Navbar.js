ce3eb1605376d6a9887dc11a68d02476a63316ba
eric <oeneeric@hotmail.com>
2022-03-29 18:44:20 -0300

    agregando navBar

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light barra_nav">
+            <a className="navbar-brand" href="#">Pañalera JyP</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Inicio</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Productos</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Zonas de entrega</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">¿Quiénes somos?</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Contacto</a>
+                </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
