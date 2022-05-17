f8d669d2c14083fa96d7b84cba15e526ce5032cd
eric <oeneeric@hotmail.com>
2022-05-17 19:20:50 -0300

    firebase 2

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,16 +1,61 @@
 import { CartContext } from './CartContext';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { collection, doc, setDoc, serverTimestamp, updateDoc, increment } from "firebase/firestore";
+import db from './firebaseConfig';
+
 
 const Cart = () => {
     const test = useContext(CartContext);
     
+    const createOrder = () => {
+        let order = {
+            buyer: {
+                name: "Juan Pedro",
+                email: "juanpedro@hotmail.com",
+                phone: "3415467812",
+            },
+            date: serverTimestamp(),
+            items: test.cartList.map(item => ({
+                id: item.idItem,
+                title: item.nameItem,
+                price: item.priceItem,
+                qty: item.qtyItem
+            })),
+            total: test.calcTotal()
+        }
+        console.log(order);
+
+        test.cartList.forEach(async (item) => {
+            const itemRef = doc(db, "products", item.idItem);
+            await updateDoc(itemRef, {
+              stock: increment(-item.qtyItem)
+            });
+          });
+    
+
+        const createOrderInFirestore = async () => {
+            // Add a new document with a generated id
+            const newOrderRef = doc(collection(db, "orders"));
+            await setDoc(newOrderRef, order);
+            return newOrderRef;
+        }
+
+        createOrderInFirestore()
+        .then(result => alert('Su orden ha sido creada. Por favor tome nota del ID de su orden.\n\n\nOrder ID: ' + result.id + '\n\n'))
+        .catch(err => console.log(err));
+    
+        test.clear();
+
+    }
+
+
     return(
         <>
         <h2>CARRITO</h2>
-        <Link to='/'><button>Seguir comprando</button></Link>
+        <Link to='/'><button>Volver al Shop</button></Link>
         {
-        (test.cartList.lengt > 0) 
+        (test.cartList.length > 0) 
         ? <button onClick={test.clear}>Vaciar carrito</button>
         : <p>Tu carrito está vacío!</p>
         }
@@ -41,7 +86,7 @@ const Cart = () => {
                 <hr />
                 <h4>Total</h4>
                 <p>${test.calcTotal()}</p>
-                <button>Finalizar compra</button>
+                <button onClick={createOrder}>Proceder con la compra</button>
                 </>
             }
         </div>
